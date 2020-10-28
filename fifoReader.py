dacabfa8be8e5215f3efb522644dd90331f1a5f4
wiwichips <wpringle@uoguelph.ca>
2020-10-27 21:05:41 -0400

    update before taking a break

--- fifoReader.py
+++ fifoReader.py
@@ -19,7 +19,7 @@ except:
   exit(0)
 
 # get the readable, writable and exceptional lists from select
-RWE = select.select([fifo_s_fd], [], [], 0.1)
+RWE = select.select([fifo_s_fd], [], [], 0.01)
 
 if RWE[0]:
   for RWE_fd in RWE: 
